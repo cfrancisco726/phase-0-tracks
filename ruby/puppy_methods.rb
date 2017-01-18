8b159fc8794fa47fe630480b83c34db8986ae0b9
cfrancisco726 <cfrancisco726@gmail.com>
2017-01-17 19:12:58 -0500

    finished release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,33 @@
 class Puppy
 
   def fetch(toy)
-    puts "I brought back the #{toy}!"
+    "I brought back the #{toy}!"
     toy
+  end
+
+  def speak(num)
+	"Woof! " * num
+  end 
+
+  def roll_over
+  	"*rolls over*"
+  end
+
+  def dog_years(i)
+  	age = i * 7
+  	age
+  end
+
+  def shake
+  	"lifts paw"
   end
 
 end
 
+spot = Puppy.new
+
+p spot.fetch("ball")
+p spot.speak(3)
+p spot.roll_over()
+p spot.dog_years(10)
+p spot.shake()
