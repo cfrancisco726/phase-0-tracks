06d5f8ec61862436f0c852ca9aba6ba20adf4bd8
cfrancisco726 <cfrancisco726@gmail.com>
2017-01-20 03:15:07 -0500

    release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,10 +1,9 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
   	words + " :)"
   end
 
@@ -12,5 +11,23 @@ end
 
 
 
-p Shout.yell_angrily("hello")
-p Shout.yelling_happily("hello")
+# p Shout.yell_angrily("hello")
+# p Shout.yelling_happily("hello")
+
+class Umpire
+	include Shout
+end
+
+class Teacher
+	include Shout
+end
+
+umpire = Umpire.new
+p umpire.yell_angrily("Strriiiiiiike")
+p umpire.yelling_happily("ball...")
+
+
+teacher = Teacher.new
+p teacher.yell_angrily("Be quiet kids")
+p teacher.yelling_happily("Goodmorning")
+
