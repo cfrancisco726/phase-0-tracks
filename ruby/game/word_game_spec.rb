9793ce383acd188e1f99b2fb6740e18c6b5c8073
cfrancisco726 <cfrancisco726@gmail.com>
2017-01-22 03:11:11 -0500

    added game folder

---
+++ ruby/game/word_game_spec.rb
@@ -0,0 +1,17 @@
+require_relative 'word_game'
+
+describe Word_game do
+  let(:word_game) { Word_game.new }
+
+  it "adds two integers" do
+    expect(calculator.add(3,4)).to eq 7
+  end
+
+  it "subtracts two integers" do
+    expect(calculator.subtract(7,1)).to eq 6
+  end
+
+  it "multiplies two integers" do
+    expect(calculator.multiply(2,3)).to eq 6
+  end
+end
