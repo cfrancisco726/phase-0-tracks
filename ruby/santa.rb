30e45834621517b158348b7b0859167e285416a3
cfrancisco726 <cfrancisco726@gmail.com>
2017-01-22 23:49:04 -0500

    cleaned up

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,6 @@
 class Santa
 
-	attr_reader :gender, :age, :ethnicity
-	attr_accessor :gender
+	attr_accessor :gender, :age, :ethnicity, :reindeer_ranking
 
 	def initialize(gender, ethnicity)
     	@gender = gender
@@ -25,7 +24,6 @@ class Santa
 	def celebrate_birthday
 		@age += 1
 
-		
 	end 
 
 	def get_mad_at(reindeer)
@@ -33,8 +31,6 @@ class Santa
 		@reindeer_ranking << @reindeer_ranking[new_reindeer]
 		@reindeer_ranking.delete_at(new_reindeer)
 		p "I am mad at #{reindeer} so he was moved to the back."
-		
-
 	end	
 
 	# # setter methods
@@ -53,22 +49,13 @@ class Santa
 
 end
 
-
-
-
-
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
-
 100.times {santas << Santa.new(example_genders[rand(example_genders.length)], example_ethnicities[rand(example_ethnicities.length)]) }
 
-
-
-
 #driver code
-reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 
 santas.each do |santa|	
 	(rand(140)).times do |age1|
@@ -76,7 +63,7 @@ santas.each do |santa|
 	end
 
 	p "This santa's age is #{santa.age} with a gender of #{santa.gender} and ethnicity of #{santa.ethnicity}"
-	bad_reindeer = reindeer_ranking[rand(reindeer_ranking.length)]
+	bad_reindeer = santa.reindeer_ranking[rand(santa.reindeer_ranking.length)]
 	santa.get_mad_at(bad_reindeer)
 
 end
