9ca463a6bb882198efbcae42f7d83c87a57b78eb
cfrancisco726 <cfrancisco726@gmail.com>
2017-01-22 21:39:59 -0500

    first test

--- ruby/game/word_game_spec.rb
+++ ruby/game/word_game_spec.rb
@@ -1,17 +1,17 @@
 require_relative 'word_game'
 
 describe Word_game do
-  let(:word_game) { Word_game.new }
-
-  it "adds two integers" do
-    expect(calculator.add(3,4)).to eq 7
+  let(:word_game) { Word_game.new("test") }
+# initialize methods
+  it "word input characters get split intoto array" do
+    expect(word_game.word_arr).to eq ["t", "e", "s", "t"]
   end
 
-  it "subtracts two integers" do
-    expect(calculator.subtract(7,1)).to eq 6
-  end
+  # it "subtracts two integers" do
+  #   expect(calculator.subtract(7,1)).to eq 6
+  # end
 
-  it "multiplies two integers" do
-    expect(calculator.multiply(2,3)).to eq 6
-  end
+  # it "multiplies two integers" do
+  #   expect(calculator.multiply(2,3)).to eq 6
+  # end
 end
