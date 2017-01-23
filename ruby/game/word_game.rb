6165731a701ddd5adc81dac7e9f5c0043d4a83ba
cfrancisco726 <cfrancisco726@gmail.com>
2017-01-22 21:35:49 -0500

    cleaned up

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -5,7 +5,7 @@ class Word_Game
 
 	def initialize(word)
 		@word_arr = word.chars
-		@new_arr = ("_ " * word_arr.length).chars
+		@new_arr = []
 		@game_count = 0
 		@game_won = false
 	end
@@ -16,27 +16,29 @@ class Word_Game
 		else
 			if @word_arr.include?(guess)
 				@word_arr.length.times do |i|
-					if @word_arr[i] = guess
-						@new_arr[i] = guess
+					if @word_arr[i] == guess
+						@new_arr.insert(i, guess)
+						@new_arr.delete_at(i + 1)
 					end
 				end
 			else 
 				puts "Incorrect entry. try again"
 			end
 			@game_count += 1
-			end
 		end
-		puts @new_arr.('join')
+		p @new_arr
 	end
 
 	def game_is_over
-		if @game_count == @word_arr.length
-			@game_won == false
-			puts "Too many tries. You chose... poorly"
-		elsif @new_arr == @word_arr
+		if @new_arr == @word_arr
 			@game_won == true
 			puts "You chose... wisely"
+		elsif @game_count == @word_arr.length
+			@game_won == false
+			puts "Too many tries. You chose... poorly"
 		end
+	end
+end
 
 
 
@@ -48,11 +50,11 @@ word_input = gets.chomp
 word_game = Word_Game.new(word_input)
 
 loop do 
-	puts "Player 2: Enter a letter guess. You only have #{@word_arr.length - @game_count} tries."
+	puts "Player 2: Enter a letter guess. You only have #{word_game.word_arr.length - word_game.game_count} tries."
 	guess = gets.chomp
 	word_game.check_guess(guess)
 	word_game.game_is_over
-	break if @game_count == @word_arr.length || @new_arr == @word_arr
+	break if word_game.game_count == word_game.word_arr.length || word_game.new_arr == word_game.word_arr
 end
 
 
