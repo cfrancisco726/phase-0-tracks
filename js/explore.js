eed833d16fc8d433b570a501efbd7ae6400d59e8
cfrancisco726 <cfrancisco726@gmail.com>
2017-01-26 01:53:41 -0500

    function works, no driver code yet

---
+++ js/explore.js
@@ -0,0 +1,18 @@
+// assign string to variable
+// break up string into characters and assign to array
+// run for loop that pushes each letter to a new array
+// then combine each letter into one word from new array
+
+var word = ("hello");
+var split_reverse = word.split("");
+// console.log(split_reverse);
+
+var reverse = [];
+
+
+ for (i = 0; i < split_reverse.length; i++){
+ 	reverse.unshift(word[i]);
+ }
+
+ console.log(reverse.join(""));
+
