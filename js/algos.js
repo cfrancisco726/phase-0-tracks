b46fe2cad7832fc526d41e7509b8049b12940654
cfrancisco726 <cfrancisco726@gmail.com>
2017-01-30 01:15:21 -0500

    logic looks ok, but syntax error

--- js/algos.js
+++ js/algos.js
@@ -1,15 +1,54 @@
 
 phrase = ["long phrase","longest phrase","longer phrase"];
-phrase_lengths = [];
+phraseLengths = [];
 
 function longest(){
 	for (var i = 0; i < phrase.length; i ++){
 		phrase_lengths.push(phrase[i].length);
 	};
-	var length_of_word = Math.max.apply(Math, phrase_lengths);
-	var longest_word = phrase_lengths.indexOf(length_of_word);
+	var length_of_word = Math.max.apply(Math, phraseLengths);
+	var longest_word = phraseLengths.indexOf(length_of_word);
 	console.log(phrase[longest_word]);
 	
 };
 
+function match(){
+
+};
+
+
+var a = {name: "Steven", age: 54};
+var b = {name: "Tamir", age: 54};
+
+
+function match(){
+
+	for (var key of Object.keys(a)) {  
+  	var aValues = a[key];
+}
+
+	for (var key of Object.keys(b)) {  
+  	var bValues = b[key];
+}
+
+	aKeys = obj.keys(a);
+	bKeys = obj.keys(b);
+
+
+	for (var i = 0; i < aKeys.length; i ++){
+		counter == 0;
+		if aValues[i] === bValues[i] && aKeys[i] === bKeys[i]{
+			return true;
+		else
+			counter += 1
+		}
+	}
+		if counter === aKeys.length{
+			return false;
+		}
+};
+
+
+// driver code
 longest();
+match();
