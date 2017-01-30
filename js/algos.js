72e020074a433763796c60707a0eca6ccebf2189
cfrancisco726 <cfrancisco726@gmail.com>
2017-01-30 03:50:41 -0500

    not printing in first if

--- js/algos.js
+++ js/algos.js
@@ -17,40 +17,43 @@ function match(){
 };
 
 
-var a = {name: "Steven", age: 54 };
-var b = {name: "Tamir", age: 54};
+var a = {name: "Steven", age: 54, height: 10};
+var b = {name: "Tamir", age: 54, weight: 10};
 
 
 function match(){
-
-
+	
 	for (var key of Object.keys(a)) {  
   	var aValues = a[key];
+  	// console.log(aValues);
 }
 
 	for (var key of Object.keys(b)) {  
   	var bValues = b[key];
+  	// console.log(bValues);
 }
 
 	aKeys = Object.keys(a);
+	// console.log(aKeys);
 	bKeys = Object.keys(b);
+	// console.log(bKeys);
 
-
-	for (var i = 0; i < aKeys.length; i ++){
+	for (var i = 0; i < aValues.length; i ++){
 		var counter = 0;
 		if (aValues[i] === bValues[i] && aKeys[i] === bKeys[i]){
+			console.log("test");
 			return true;
 		}else{
 			counter += 1
 			return counter;
 			console.log(counter);
 		}
-	}
+	};
 		if (counter === aKeys.length){
 			console.log("false");
 			return false;
 		}else{
-			consolelog("true");
+			console.log("true");
 			return true;
 		}
 
