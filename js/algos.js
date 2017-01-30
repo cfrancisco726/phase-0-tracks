a66ec71b4b310d93daf7db8dc8f414bf6e53a2a4
cfrancisco726 <cfrancisco726@gmail.com>
2017-01-30 01:58:06 -0500

    cleaned up but still not working

--- js/algos.js
+++ js/algos.js
@@ -4,7 +4,7 @@ phraseLengths = [];
 
 function longest(){
 	for (var i = 0; i < phrase.length; i ++){
-		phrase_lengths.push(phrase[i].length);
+		phraseLengths.push(phrase[i].length);
 	};
 	var length_of_word = Math.max.apply(Math, phraseLengths);
 	var longest_word = phraseLengths.indexOf(length_of_word);
@@ -17,11 +17,12 @@ function match(){
 };
 
 
-var a = {name: "Steven", age: 54};
+var a = {name: "Steven", age: 54 };
 var b = {name: "Tamir", age: 54};
 
 
 function match(){
+
 
 	for (var key of Object.keys(a)) {  
   	var aValues = a[key];
@@ -31,22 +32,33 @@ function match(){
   	var bValues = b[key];
 }
 
-	aKeys = obj.keys(a);
-	bKeys = obj.keys(b);
+	aKeys = Object.keys(a);
+	bKeys = Object.keys(b);
 
 
 	for (var i = 0; i < aKeys.length; i ++){
-		counter == 0;
-		if aValues[i] === bValues[i] && aKeys[i] === bKeys[i]{
+		var counter = 0;
+		if (aValues[i] === bValues[i] && aKeys[i] === bKeys[i]){
 			return true;
-		else
+		}else{
 			counter += 1
+			console.log(counter);
 		}
 	}
-		if counter === aKeys.length{
+		if (counter === aKeys.length){
+			console.log("false");
 			return false;
+		}else{
+			consolelog("true");
+			return true;
 		}
+
 };
+
+// genArr = []
+// function generate(num){
+
+// }
 
 
 // driver code
